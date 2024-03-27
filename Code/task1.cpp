9d0d482967faa2dc63581357ad23f6d6fe587538
Zuoliang Liao <zuoliangliao@gmail.com>
2024-03-28 00:39:16 +1100

    1

--- Code/task1.cpp
+++ Code/task1.cpp
@@ -1,25 +1,186 @@
-// Find bounding box for triangle
-    float minX = std::min({v[0].x(), v[1].x(), v[2].x()});
-    float maxX = std::max({v[0].x(), v[1].x(), v[2].x()});
-    float minY = std::min({v[0].y(), v[1].y(), v[2].y()});
-    float maxY = std::max({v[0].y(), v[1].y(), v[2].y()});
+#if __INTELLISENSE__
+#undef __ARM_NEON
+#undef __ARM_NEON__
+#endif
+#include <cstddef>
+#include <iostream>
+#include <opencv2/core/matx.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/opencv.hpp>
 
-//Iterate
-    for(int x = minX;x<=maxX;x++){
-        for(int y = minY;y<=maxY;y++){
-            if(insideTriangle(x+0.5,y+0.5,t.v)){
-                auto[alpha, beta, gamma] = computeBarycentric2D(x+0.5, y+0.5, t.v);
-                float w_reciprocal = 1.0/(alpha/v[0].w()+beta/v[1].w()+gamma/v[2].w());
-                float z_interpolated = alpha*v[0].z()/v[0].w()+beta*v[1].z()/v[1].w()+gamma*v[2].z()/v[2].w();
-                z_interpolated*=w_reciprocal;
-                //如果当前位置深度比depth_buf更小，则更新颜色值
-                if(z_interpolated<depth_buf[get_index(x,y)]){
-                    Eigen::Vector3f interpolated_color = interpolate(alpha,beta,gamma,t.color[0],t.color[1]*255,t.color[2]*255,1);
-                    set_pixel(Vector2i(x, y), interpolated_color);
-                    depth_buf[get_index(x,y)]=z_interpolated;
-                    
-                }
-            }
-        }
+#include "Eigen/Dense"
+#include "global.hpp"
+#include "mesh.hpp"
+#include "rasterizer.hpp"
+
+Eigen::Matrix4f get_rotation(float rotation_angle, const Eigen::Vector3f &axis) {
+    // Calculate a rotation matrix from rotation axis and angle.
+    // Note: rotation_angle is in degree.
+    Eigen::Matrix4f rotation_matrix = Eigen::Matrix4f::Identity();
+
+    float rotation_angle_rad = rotation_angle * MY_PI / 180.0;
+    float cos_theta = cos(rotation_angle_rad);
+    float sin_theta = sin(rotation_angle_rad);
+
+    Eigen::Vector3f axis_ = axis.normalized();
+    Eigen::Matrix3f identity = Eigen::Matrix3f::Identity();
+    Eigen::Matrix3f ux;
+    ux << 0, -axis_.z(), axis_.y(), axis_.z(), 0, -axis_.x(), -axis_.y(), axis_.x(), 0;
+
+    Eigen::Matrix3f rotation_matrix_3x3 =
+        cos_theta * identity + (1 - cos_theta) * (axis_ * axis_.transpose()) + sin_theta * ux;
+    rotation_matrix.block<3, 3>(0, 0) = rotation_matrix_3x3;
+
+    return rotation_matrix;
+}
+
+Eigen::Matrix4f get_translation(const Eigen::Vector3f &translation) {
+    // Calculate a transformation matrix of given translation vector.
+    Eigen::Matrix4f trans = Eigen::Matrix4f::Identity();
+    trans(0, 3) = translation.x();
+    trans(1, 3) = translation.y();
+    trans(2, 3) = translation.z();
+    return trans;
+}
+
+Eigen::Matrix4f look_at(Eigen::Vector3f eye_pos, Eigen::Vector3f target,
+                        Eigen::Vector3f up = Eigen::Vector3f(0, 1, 0)) {
+    Eigen::Matrix4f view = Eigen::Matrix4f::Identity();
+
+    Eigen::Vector3f z = (eye_pos - target).normalized();
+    Eigen::Vector3f x = up.cross(z).normalized();
+    Eigen::Vector3f y = z.cross(x).normalized();
+
+    Eigen::Matrix4f rotate;
+    rotate << x.x(), x.y(), x.z(), 0, y.x(), y.y(), y.z(), 0, z.x(), z.y(), z.z(), 0, 0, 0, 0, 1;
+
+    Eigen::Matrix4f translate;
+    translate << 1, 0, 0, -eye_pos[0], 0, 1, 0, -eye_pos[1], 0, 0, 1, -eye_pos[2], 0, 0, 0, 1;
+
+    view = rotate * translate * view;
+    return view;
+}
+
+Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos) {
+    Eigen::Matrix4f view = Eigen::Matrix4f::Identity();
+
+    view = look_at(eye_pos, Eigen::Vector3f(0, 0, 0), Eigen::Vector3f(0, 1, 0));
+
+    return view;
+}
+
+Eigen::Matrix4f get_model_matrix(float rotation_angle, const Eigen::Vector3f &axis,
+                                 const Eigen::Vector3f &translation) {
+    Eigen::Matrix4f model = Eigen::Matrix4f::Identity();
+
+    Eigen::Matrix4f rotation = get_rotation(rotation_angle, axis);
+
+    Eigen::Matrix4f trans = get_translation(translation);
+
+    model = trans * rotation * model;
+    return model;
+}
+
+Eigen::Matrix4f get_projection_matrix(float eye_fovy, float aspect_ratio, float zNear, float zFar) {
+    // Create the projection matrix for the given parameters.
+    // Then return it.
+    Eigen::Matrix4f projection = Eigen::Matrix4f::Identity();
+
+    float eye_fovy_rad = eye_fovy * MY_PI / 180.0;
+    float top = zNear * tan(eye_fovy_rad / 2.0);
+    float bottom = -top;
+    float right = top * aspect_ratio;
+    float left = -right;
+
+    projection << zNear / right, 0, 0, 0, 0, zNear / top, 0, 0, 0, 0, (zNear + zFar) / (zNear - zFar),
+        2 * zNear * zFar / (zNear - zFar), 0, 0, -1, 0;
+
+    return projection;
+}
+
+int main(int argc, const char **argv) {
+    float angle = 140;
+    bool command_line = false;
+    bool culling = false;
+    bool anti_aliasing = false;
+    std::string filename = "output.png";
+
+    if (argc == 2) {
+        command_line = true;
+        filename = std::string(argv[1]);
     }
 
+    // load house mesh
+    Mesh house_mesh;
+    if (!house_mesh.load_obj("../models/house.obj")) {
+        std::cerr << "Failed to load house mesh." << std::endl;
+        return -1;
+    }
+
+    // manually assign some colors for the house
+    // roof {240, 70, 80}
+    // base {73, 182, 109}
+    // chimney {93, 100, 38}
+    std::vector<Eigen::Vector3f> cols;
+    for (size_t i = 0; i < 6; i++) {
+        cols.push_back({240, 70, 80});
+    }
+    for (size_t i = 0; i < 8; i++) {
+        cols.push_back({73, 182, 109});
+    }
+    for (size_t i = 0; i < 8; i++) {
+        cols.push_back({93, 100, 38});
+    }
+
+    rst::rasterizer r(700, 700);
+    Eigen::Vector3f eye_pos = {0, 0, 10};
+
+    auto pos_id = r.load_positions(house_mesh.vertices);
+    auto ind_id = r.load_indices(house_mesh.faces);
+    auto col_id = r.load_colors(cols);
+
+    int key = 0;
+    int frame_count = 0;
+
+    if (command_line) {
+        r.clear(rst::Buffers::Color | rst::Buffers::Depth);
+
+        r.set_model(get_model_matrix(angle, {0, 1, 0}, {0, 0, 0}));
+        r.set_view(get_view_matrix(eye_pos));
+        r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
+
+        r.draw(pos_id, ind_id, col_id, rst::Primitive::Triangle, culling, anti_aliasing);
+        cv::Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
+        image.convertTo(image, CV_8UC3, 1.0f);
+        cv::cvtColor(image, image, cv::COLOR_RGB2BGR);
+        cv::imwrite(filename, image);
+
+        return 0;
+    }
+
+    while (key != 27) {
+        r.clear(rst::Buffers::Color | rst::Buffers::Depth);
+
+        r.set_model(get_model_matrix(angle, {0, 1, 0}, {0, 0, 0}));
+        r.set_view(get_view_matrix(eye_pos));
+        r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
+
+        r.draw(pos_id, ind_id, col_id, rst::Primitive::Triangle, culling, anti_aliasing);
+
+        cv::Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
+        image.convertTo(image, CV_8UC3, 1.0f);
+        cv::cvtColor(image, image, cv::COLOR_RGB2BGR);
+        cv::imshow("image", image);
+
+        key = cv::waitKey(10);
+        if (key == 'j')
+            angle += 10;
+        else if (key == 'k')
+            angle -= 10;
+
+        std::cout << "frame count: " << frame_count++ << '\n';
+    }
+
+    return 0;
+}
